9aa973cc0ab3d196e836a63750ccae0a8f069a59
Mamoka1104 <109488428+Mamoka1104@users.noreply.github.com>
2023-03-04 00:55:06 +1000

    add Добавили файл JS.

---
+++ fetured_item.js
@@ -0,0 +1,37 @@
+const dataFI = `[
+{
+    "photo": "img/feturedItemphoto2.jpg",
+    "alt" : "photo2",
+    "name": "ellery x m'o capsule",
+    "text": "Known for her sculptural takes on&nbsp;traditional tailoring, Australian arbiter of&nbsp;cool Kym Ellery teams up&nbsp;with Moda Operandi",
+    "prise": "52.00"
+},
+{
+    "photo": "img/feturedItemphoto3.jpg",
+    "alt" : "photo3",
+    "name": "ellery x m'o capsule",
+    "text": "Known for her sculptural takes on&nbsp;traditional tailoring, Australian arbiter of&nbsp;cool Kym Ellery teams up&nbsp;with Moda Operandi",
+    "prise": "52.00"
+},
+{
+    "photo": "img/feturedItemphoto4.jpg",
+    "alt" : "photo4",
+    "name": "ellery x m'o capsule",
+     "text": "Known for her sculptural takes on&nbsp;traditional tailoring, Australian arbiter of&nbsp;cool Kym Ellery teams up&nbsp;with Moda Operandi",
+    "prise": "52.00"
+},
+{
+    "photo": "img/feturedItemphoto5.jpg",
+    "alt" : "photo5",
+    "name": "ellery x m'o capsule",
+        "text": "Known for her sculptural takes on&nbsp;traditional tailoring, Australian arbiter of&nbsp;cool Kym Ellery teams up&nbsp;with Moda Operandi",
+            "prise": "52.00"
+    },
+{
+    "photo": "img/feturedItemphoto6.jpg",
+    "alt" : "photo6",
+    "name": "ellery x m'o capsule",
+        "text": "Known for her sculptural takes on&nbsp;traditional tailoring, Australian arbiter of&nbsp;cool Kym Ellery teams up&nbsp;with Moda Operandi",
+            "prise": "52.00"
+}
+]`
